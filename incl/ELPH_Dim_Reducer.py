325f6e605176c20679d52f6472eeb502d5898aa7
Stefan Meinecke <meinecke@tu-berlin.de>
2022-05-24 15:28:49 +0200

    added FFT dimension reduction. To be debugged and tested

--- incl/ELPH_Dim_Reducer.py
+++ incl/ELPH_Dim_Reducer.py
@@ -31,3 +31,29 @@ class SVD(base_dim_reducer):
     def expand(self, coef_matrix):
         dim = coef_matrix.shape[0]
         return self.U[:,:dim] @ coef_matrix 
+      
+      
+class FFT(base_dim_reducer):
+    def __init__(self):
+        pass
+    
+    def train(self, data_matrix):
+        self.full_dim = data_matrix.shape[0]
+        
+    def reduce(self, data_matrix, prdim):
+        
+        FT = np.fft.rfft(data_matrix, axis=0)[:prdim]
+        
+        real_matrix = np.concatenate((np.real(FT),np.imag(FT)[1:]), axis=0)
+        
+        return real_matrix
+    
+    def expand(self, coef_matrix):
+        
+        ind_split = (coef_matrix.shape[0]+1)//2
+        
+        complex_matrix = coef_matrix[:ind_split] + 1.j*np.concatenate( (np.zeros((1,coef_matrix.shape[1])), coef_matrix[ind_split:]), axis=0) 
+        
+        iFT = np.fft.irfft(complex_matrix, n=self.full_dim, axis=0)
+        
+        return iFT
