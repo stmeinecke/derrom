5ff99eb7a08d37c4540620456d12b22df4d37968
Stefan Meinecke <meinecke@tu-berlin.de>
2022-09-19 14:47:17 +0200

    updated all plot scripts in the figs folder to work with narrom. About ready to be merged

--- incl/narrom_utils.py
+++ incl/narrom_utils.py
@@ -73,6 +73,7 @@ def load_trajectories(filename='../trajectories.npz'):
 #######################################
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 def plot_trajectory(data,title='trajectory'):
     plt.imshow(data, aspect='auto', interpolation='none',origin='lower',cmap='Reds')
@@ -82,37 +83,13 @@ def plot_trajectory(data,title='trajectory'):
     cb = plt.colorbar()
     cb.set_label('occupation')
     plt.show()
-    
 
-import matplotlib.colors as colors
-class MidpointNormalize(colors.Normalize):
-    def __init__(self, vmin=None, vmax=None, vcenter=None, clip=False):
-        self.vcenter = vcenter
-        colors.Normalize.__init__(self, vmin, vmax, clip)
-
-    def __call__(self, value, clip=False):
-        """
-        Map value to the interval [0, 1]. The clip argument is unused.
-        """
-        result, is_scalar = self.process_value(value)
-        self.autoscale_None(result)  # sets self.vmin, self.vmax if None
-
-        if not self.vmin <= self.vcenter <= self.vmax:
-            raise ValueError("vmin, vcenter, vmax must increase monotonically")
-        result = np.ma.masked_array(
-            np.interp(result, [self.vmin, self.vcenter, self.vmax],
-                      [0, 0.5, 1.]), mask=np.ma.getmask(result))
-        if is_scalar:
-            result = np.atleast_1d(result)[0]
-        return result
     
 def plot_difference(test,truth,title='difference'):
     
     err = test-truth
-
-    midnorm = MidpointNormalize(vmin=err.min(), vcenter=0.0, vmax=err.max())
     
-    plt.imshow(err, aspect='auto', interpolation='none',origin='lower',cmap='bwr', norm=midnorm)
+    plt.imshow(err, aspect='auto', interpolation='none',origin='lower',cmap='bwr', norm=colors.CenteredNorm(vcenter=0.0))
     plt.title(title)
     plt.xlabel(r'time $n_k$')
     plt.ylabel(r'electron momentum $n_t$')
