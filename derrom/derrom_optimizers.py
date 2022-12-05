5a1912449650d310398ceefe4503cbc6fbe77504
Stefan Meinecke <meinecke@tu-berlin.de>
2022-12-05 15:44:29 +0100

    hm, who knows what happened

--- derrom/derrom_optimizers.py
+++ derrom/derrom_optimizers.py
@@ -15,14 +15,17 @@ class lstsqrs(base_optimizer):
     
     def solve(self, feature_matrix, target_matrix):
         return np.asarray( np.linalg.lstsq(feature_matrix, target_matrix, rcond = -1)[0] )
+      
+      
 
-    
 class ridge(base_optimizer):
     def __init__(self, alpha = 1e-6):
         self.alpha = alpha
-        
+        print('testo')
+    
     def solve(self, feature_matrix, target_matrix):
         return np.linalg.inv(feature_matrix.T @ feature_matrix + self.alpha * np.identity(feature_matrix.shape[1])) @ feature_matrix.T @ target_matrix
+
 
   
 
